827dc98c4508fd347f2020d5c688badb6d19cbd2
PacelliV <flores.eugenio03@gmail.com>
2022-12-07 13:46:13 -0500

    new base url

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import { defineConfig } from "vite"
 export default defineConfig({
     plugins: [],
     base: "/movie-watchlist/",
-    base: "movie-watchlist/src/watchlist/index.html",
+    base: "/movie-watchlist/src/watchlist/index.html",
 })
