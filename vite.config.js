51ede14b86428e074520385ead8dab59d9607b90
PacelliV <flores.eugenio03@gmail.com>
2022-12-08 11:20:57 -0500

    endpoint added

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import { defineConfig } from "vite"
 export default defineConfig({
     plugins: [],
     base: "/movie-watchlist/",
-    base: "/movie-watchlist/src/watchlist/index.html/",
+    endpoint: "/src/watchlist/index.html/",
 })
