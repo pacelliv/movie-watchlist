1b20d424727c1d790ac28db5d75f063e8757ab2d
PacelliV <flores.eugenio03@gmail.com>
2022-12-08 13:09:23 -0500

    new order

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, "index.html"),
-                nested: resolve(__dirname, "src/watchlist/watchlist.html"),
+                watchlist: resolve(__dirname, "src/watchlist/watchlist.html"),
             },
         },
     },
